3f73d7350721d654bc426a28fc852f55b463b1bb
aksilont <aksilont@gmail.com>
2023-02-16 22:00:01 +0300

    Первый этап верстки кодом экрана Избранное

---
+++ MyNews/Flow/Main/Favorite/FavoriteCollectionViewCell.swift
@@ -0,0 +1,105 @@
+//
+//  FavoriteCollectionViewCell.swift
+//  MyNews
+//
+//  Created by Aksilont on 16.02.2023.
+//
+
+import UIKit
+
+class FavoriteCollectionViewCell: UICollectionViewCell {
+    
+    // MARK: - Properties
+    
+    private let imageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.contentMode = .scaleAspectFill
+        imageView.layer.masksToBounds = true
+        return imageView
+    }()
+    
+    private let dateLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 13)
+        return label
+    }()
+    private let likeButton: LikeButton = {
+        let button = LikeButton()
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.heightAnchor.constraint(equalToConstant: 40).isActive = true
+        button.widthAnchor.constraint(equalToConstant: 40).isActive = true
+        return button
+    }()
+    private let stackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.axis = .horizontal
+        return stackView
+    }()
+    
+    private let titleLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.numberOfLines = 2
+        label.font = UIFont.systemFont(ofSize: 16, weight: .bold)
+        return label
+    }()
+    
+    // MARK: - Init
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        setupUI()
+        setupConstraints()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        
+        setupUI()
+        setupConstraints()
+    }
+    
+    // MARK: - Methods
+    
+    private func setupUI() {
+        contentView.backgroundColor = .systemBackground
+        contentView.layer.cornerRadius = 22
+        contentView.layer.cornerCurve = .continuous
+        contentView.layer.masksToBounds = true
+        
+        stackView.addArrangedSubview(dateLabel)
+        stackView.addArrangedSubview(likeButton)
+        
+        contentView.addSubview(imageView)
+        contentView.addSubview(stackView)
+        contentView.addSubview(titleLabel)
+    }
+    
+    private func setupConstraints() {
+        let safeArea = contentView.safeAreaLayoutGuide
+        NSLayoutConstraint.activate([
+            imageView.topAnchor.constraint(equalTo: safeArea.topAnchor),
+            imageView.leftAnchor.constraint(equalTo: safeArea.leftAnchor),
+            imageView.rightAnchor.constraint(equalTo: safeArea.rightAnchor),
+            imageView.heightAnchor.constraint(equalTo: safeArea.heightAnchor, multiplier: 0.5),
+            
+            stackView.topAnchor.constraint(equalTo: imageView.bottomAnchor),
+            stackView.leftAnchor.constraint(equalTo: safeArea.leftAnchor, constant: 6),
+            stackView.rightAnchor.constraint(equalTo: safeArea.rightAnchor, constant: -6),
+            
+            titleLabel.topAnchor.constraint(equalTo: stackView.bottomAnchor),
+            titleLabel.leftAnchor.constraint(equalTo: safeArea.leftAnchor, constant: 6),
+            titleLabel.rightAnchor.constraint(equalTo: safeArea.rightAnchor, constant: -6),
+        ])
+    }
+    
+    func update(news: News) {
+        imageView.image = news.image
+        dateLabel.text = news.date.simpleFormat()
+        likeButton.liked = news.liked
+        titleLabel.text = news.title
+    }
+}
--- MyNews/Flow/Main/Favorite/FavoriteViewController.swift
+++ MyNews/Flow/Main/Favorite/FavoriteViewController.swift
@@ -8,9 +8,88 @@
 import UIKit
 
 class FavoriteViewController: UIViewController {
-
+    
+    // MARK: - Properties
+    private let news: [News] = News.mockData()
+    private var collectionView: UICollectionView!
+    
+    // MARK: - Life Cycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        setupView()
+        setupConstraints()
+    }
+    
+    
+    // MARK: - Methods
+    
+    private func setupView() {
+        view.backgroundColor = UIColor(named: "BackgroundColor")
+        
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .vertical
+        
+        collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        collectionView.register(FavoriteCollectionViewCell.self, forCellWithReuseIdentifier: "cell")
+        collectionView.contentInset = UIEdgeInsets(top: 5, left: 15, bottom: 5, right: 15)
+                
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        collectionView.backgroundColor = .clear
+        collectionView.showsVerticalScrollIndicator = false
+        view.addSubview(collectionView)
+    }
+    
+    private func setupConstraints() {
+        let safeArea = view.safeAreaLayoutGuide
+        NSLayoutConstraint.activate([
+            collectionView.topAnchor.constraint(equalTo: safeArea.topAnchor),
+            collectionView.leftAnchor.constraint(equalTo: safeArea.leftAnchor),
+            collectionView.rightAnchor.constraint(equalTo: safeArea.rightAnchor),
+            collectionView.bottomAnchor.constraint(equalTo: safeArea.bottomAnchor),
+        ])
+    }
+}
+
+
+extension FavoriteViewController: UICollectionViewDelegate {
+}
+
+extension FavoriteViewController: UICollectionViewDataSource {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        news.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(
+            withReuseIdentifier: "cell",
+            for: indexPath) as? FavoriteCollectionViewCell else {
+            fatalError("Wrong cell")
+        }
+        cell.update(news: news[indexPath.row])
+        return cell
+    }
+    
+}
+
+extension FavoriteViewController: UICollectionViewDelegateFlowLayout {
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let width = (collectionView.bounds.width - 15 * 2 - 18) / 2
+        return CGSize(width: width, height: width * 1.2)
     }
 
+//    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+//        return UIEdgeInsets(top: 5, left: 24, bottom: 5, right: 24)
+//    }
+    
+    func collectionView(_ collectionView: UICollectionView,
+                        layout collectionViewLayout: UICollectionViewLayout,
+                        minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 24
+    }
 }
